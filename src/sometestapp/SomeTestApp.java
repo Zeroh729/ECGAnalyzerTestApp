cfa05ce90784d9ece7bd4fc3466bc995ac3f0c92
zeroh729 <zeroh729@gmail.com>
2018-05-16 11:55:37 +0800

    Refactored ECGAnalyzerLibrary

--- src/sometestapp/SomeTestApp.java
+++ src/sometestapp/SomeTestApp.java
@@ -1,8 +1,10 @@
 package sometestapp;
 
+import ecganal.Callback.ECGAnalysisCallback;
 import ecganal.ECGAnalyzer;
 import ecganal.DSPFunction;
 import ecganal.DSPFunction.Type;
+import ecganal.Model.ECGSummary;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -12,23 +14,18 @@ public class SomeTestApp {
     
     public static void main(String[] args) {
         filedata = DummyData.getData();
-        ECGAnalyzer.setData(filedata);
-        data = ECGAnalyzer.getData();
-        
-        Dataviz.drawData("Original Data", "milliseconds", "volts", data);
-//        data = DSPFunction.execute(Type.LOWPASSFILTER, data);
-//        Dataviz.drawData("Low Pass Filter", "milliseconds", "volts", data);
-//        data = DSPFunction.execute(Type.HIGHPASSFILTER, data);
-//        Dataviz.drawData("High Pass Filter", "milliseconds", "volts", data);
-////        data = DSPFunction.execute(Type.DERIVATIVE, data);
-////        Dataviz.drawData("Derivative", "milliseconds", "volts", data);
-//        data = DSPFunction.execute(Type.SQUARING, data);
-//        Dataviz.drawData("Squaring", "milliseconds", "volts", data);
-        data = DSPFunction.execute(Type.ALL, data);
-        Dataviz.drawData("All", "milliseconds", "volts", data);
-        DSPFunction.detectRPeaks();
-        int bpm = DSPFunction.calculateBPM();
-        System.out.println("BPM is " + bpm);
+        System.out.println("Processing...");
+        ECGAnalyzer.getInstance().analyzeData(filedata, new ECGAnalysisCallback(){
+            @Override
+            public void onSuccess(ECGSummary summary) {
+                System.out.println("BPM is " + summary.getBPM());
+            }
+            
+            @Override
+            public void onFail(String message) {    
+                System.out.println("Fail! " + message);
+            }
+        });
     }
 
 }
